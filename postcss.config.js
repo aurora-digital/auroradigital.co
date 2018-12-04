cd946300a2b987ba96aa497d05eba60fcd9a0895
José Sousa <josesousa9000@gmail.com>
2018-12-04 12:13:23 +0000

    wip

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: {
     "postcss-import": {},
     precss: {},
-    "postcss-calc": {},
     "postcss-modules": {
       getJSON: () => {},
       generateScopedName: "[folder]__[local]__[hash]",
