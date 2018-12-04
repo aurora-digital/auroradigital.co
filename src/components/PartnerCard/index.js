ec102141903b44bf6d26a500511a2ba66f4cbd5c
filipebfernandes <f92fernandes@gmail.com>
2018-12-04 11:07:16 +0000

    add PartnerCard component

--- src/components/PartnerCard/index.js
+++ src/components/PartnerCard/index.js
@@ -2,34 +2,40 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames/bind";
 import Typography from "../Typography";
+import linkedin from "../../assets/images/in.svg";
 
-import roberto from "../../assets/images/roberto.png";
 import "./index.css";
 
-const PartnerCard = ({ name, onClick, selected }) => {
+const PartnerCard = ({ onClick, selected, partner }) => {
   const rootStyles = classNames("root", {
     selected,
   });
 
   return (
-    <button styleName={rootStyles} onClick={onClick} name={name}>
+    <button styleName={rootStyles} onClick={onClick}>
       <div styleName="content">
-        <img alt="partner" styleName="image" src={roberto} />
-        <Typography weight="bold">Roberto Machado</Typography>
-        <Typography>CEO @Subvisual</Typography>
-
-        <div styleName="linkedin">in</div>
+        <img alt="partner" styleName="image" src={partner.image} />
+        <Typography weight="bold">{partner.name}</Typography>
+        <Typography>{partner.position}</Typography>
+        <a
+          styleName="linkedin"
+          href={partner.linkedin}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} alt="linkedin" />
+        </a>
       </div>
     </button>
   );
 };
 
 PartnerCard.propTypes = {
-  name: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   selected: PropTypes.bool,
   partner: PropTypes.shape({
     name: PropTypes.string.isRequired,
+    position: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
     linkedin: PropTypes.string.isRequired,
   }).isRequired,
