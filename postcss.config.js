b501923f8f94ce9e92317c8aa2496ddcfa2ca28d
José Sousa <josesousa9000@gmail.com>
2018-12-04 12:12:57 +0000

    Fix section width

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,7 @@ module.exports = {
   plugins: {
     "postcss-import": {},
     precss: {},
+    "postcss-calc": {},
     "postcss-modules": {
       getJSON: () => {},
       generateScopedName: "[folder]__[local]__[hash]",
