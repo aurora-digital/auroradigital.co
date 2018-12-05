acbde6ba330c93b1329f4c6f604d4c9865cdc345
José Francisco Sousa <josesousa9000@gmail.com>
2018-12-05 14:36:35 +0000

    Add hero section to homepage
    * Multiple small changes on Section component to adjust for responsive design.
    * Add overlay to background image with mix-blend-mode

---
+++ src/components/Project/index.js
@@ -0,0 +1,45 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Img from "gatsby-image";
+import Typography from "root/components/Typography";
+
+import "./index.css";
+
+const Project = ({ image, projectName, projectUrl }) => (
+  <a
+    styleName="root"
+    tabIndex="0"
+    href={projectUrl}
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <div styleName="title">
+      <Typography
+        variant="h2"
+        color="white"
+        weight="bold"
+        fontFamily="meta-serif"
+      >
+        {projectName}
+      </Typography>
+    </div>
+    <Img
+      styleName="image"
+      fluid={image}
+      alt={`${projectName}-project`.toLowerCase()}
+    />
+    <div styleName="project-link">
+      <Typography variant="body" color="white" weight="bold">
+        View Project
+      </Typography>
+    </div>
+  </a>
+);
+
+Project.propTypes = {
+  image: PropTypes.shape({}).isRequired,
+  projectName: PropTypes.string.isRequired,
+  projectUrl: PropTypes.string.isRequired,
+};
+
+export default Project;
---
+++ src/sections/HomeHero/withQuery.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StaticQuery, graphql } from "gatsby";
+
+const query = graphql`
+  query {
+    oncostats: file(relativePath: { eq: "images/oncostats.png" }) {
+      image: childImageSharp {
+        fluid(maxHeight: 320) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+
+    sioslife: file(relativePath: { eq: "images/sioslife.png" }) {
+      image: childImageSharp {
+        fluid(maxHeight: 320) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+  }
+`;
+
+export default Component => () => (
+  <StaticQuery query={query} render={data => <Component data={data} />} />
+);
