c9585edf3be58c2389efeafe6ceb285291df627b
José Francisco Sousa <josesousa9000@gmail.com>
2018-12-05 14:40:24 +0000

    Optimize background images using Gatsby Image

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -1,26 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Img from "gatsby-image";
 
 import "./index.css";
 
+const imageContainerStyle = {
+  position: `absolute`,
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  height: "100%",
+  width: "100%",
+  backgroundColor: "#503875",
+  zIndex: -1,
+};
+
+const imageStyle = { opacity: 0.9, mixBlendMode: "normal" };
+
 const Background = ({ children, image }) => (
-  <div
-    styleName="root"
-    style={{
-      backgroundImage: `url(${image})`,
-    }}
-  >
+  <div styleName="root">
+    <Img
+      styleName="background-image"
+      style={imageContainerStyle}
+      imgStyle={imageStyle}
+      fluid={image}
+      critical
+    />
     {children}
   </div>
 );
 
 Background.propTypes = {
-  image: PropTypes.string,
+  image: PropTypes.shape({}).isRequired,
   children: PropTypes.node.isRequired,
-};
-
-Background.defaultProps = {
-  image: false,
 };
 
 export default Background;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,40 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { graphql } from "gatsby";
 import Navbar from "root/components/Navbar";
 import Background from "root/components/Background";
-import heroMolecules from "root/assets/images/hero-molecules.png";
 import Layout from "root/components/Layout";
 import HomePageHero from "root/sections/HomeHero";
 
-const IndexPage = () => (
+export const query = graphql`
+  query {
+    heroMolecules: file(relativePath: { eq: "images/hero-molecules.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 4000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <Background image={heroMolecules}>
+    <Background image={data.heroMolecules.image.fluid}>
       <Navbar currentPage="home" />
       <HomePageHero />
     </Background>
   </Layout>
 );
+
+IndexPage.propTypes = {
+  data: PropTypes.shape({
+    heroMolecules: PropTypes.shape({
+      image: PropTypes.shape({
+        fluid: PropTypes.shape({}).isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
 
 export default IndexPage;
