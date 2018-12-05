b9a799627ae745b937c2583b0cc305703fa0e64e
José Sousa <josesousa9000@gmail.com>
2018-12-05 11:15:51 +0000

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { graphql } from "gatsby";
 import Navbar from "root/components/Navbar";
 import Background from "root/components/Background";
-import heroMolecules from "root/assets/images/hero-molecules.png";
 import Layout from "root/components/Layout";
 import HomePageHero from "root/sections/HomeHero";
 
-const IndexPage = () => (
+export const query = graphql`
+  query {
+    heroMolecules: file(relativePath: { eq: "images/hero-molecules.png" }) {
+      image: childImageSharp {
+        fluid(maxWidth: 4000) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
+
+const IndexPage = ({ data }) => (
   <Layout>
-    <Background image={heroMolecules}>
+    <Background image={data.heroMolecules.image.fluid}>
       <Navbar currentPage="home" />
       <HomePageHero />
     </Background>
