252d92d20b02823eda171980a639182e638a879c
Tânia Rocha <taniapassosrocha@gmail.com>
2018-12-06 14:00:23 +0000

    Add team elements

---
+++ src/components/TeamElement/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Typography from "root/components/Typography";
+import Img from "gatsby-image";
+
+import "./index.css";
+
+const TeamElement = ({ name, position, image }) => (
+  <button styleName="root">
+    <div styleName="image">
+      <div styleName="rect" />
+      <Img alt={`${name.toLowerCase()} photo`} fluid={image} critical />
+    </div>
+    <div styleName="name">
+      <Typography variant="h3" color="dark-blue">
+        {name}
+      </Typography>
+    </div>
+    <div styleName="position">
+      <Typography color="dark-blue">{position}</Typography>
+    </div>
+  </button>
+);
+
+TeamElement.propTypes = {
+  name: PropTypes.string.isRequired,
+  position: PropTypes.string.isRequired,
+  image: PropTypes.shape({}).isRequired,
+};
+
+export default TeamElement;
