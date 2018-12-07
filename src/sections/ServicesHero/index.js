ab0be784e332b1b7dcb9c88729f576b5bc982bab
filipebfernandes <f92fernandes@gmail.com>
2018-12-07 16:54:54 +0000

    wip

--- src/sections/ServicesHero/index.js
+++ src/sections/ServicesHero/index.js
@@ -1,31 +1,33 @@
 import React from "react";
 import Section from "root/components/Section";
 import BackgroundVideoText from "root/components/BackgroundVideoText";
-import { isMobile, isTablet } from "react-device-detect";
 
 const column = 114;
 const gutter = 28;
 const eightColumns = 8 * column + 7 * gutter;
-
 const sixColumns = 6 * column + 5 * gutter;
+const breakpointMobile = 768;
+const breakpointDesktop = 1268;
 
-const renderBackgroundVideoText = () => {
-  if (isTablet) {
+const ServicesHero = () => {
+  const width = document.documentElement.clientWidth;
+
+  if (width >= breakpointDesktop) {
     return (
       <Section>
         <BackgroundVideoText
-          key="tablet"
-          labels={["A dynamic team to", "design and develop", "your product"]}
+          key="desktop"
+          labels={["A dynamic team to design", "and develop your product"]}
           poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/oceanshot.jpg"
           video="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm"
-          height="330px"
-          width={`${sixColumns}px`}
+          height="195px"
+          width={`${eightColumns}px`}
         />
       </Section>
     );
   }
 
-  if (isMobile) {
+  if (width < breakpointMobile) {
     return (
       <Section>
         <div>Implement Mobile</div>
@@ -36,17 +38,15 @@ const renderBackgroundVideoText = () => {
   return (
     <Section>
       <BackgroundVideoText
-        key="desktop"
-        labels={["A dynamic team to design", "and developyour product"]}
+        key="tablet"
+        labels={["A dynamic team to", "design and develop", "your product"]}
         poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/oceanshot.jpg"
         video="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm"
         height="330px"
-        width={`${eightColumns}px`}
+        width={`${sixColumns}px`}
       />
     </Section>
   );
 };
-
-const ServicesHero = () => renderBackgroundVideoText();
 
 export default ServicesHero;
