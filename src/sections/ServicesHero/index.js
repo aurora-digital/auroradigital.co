c4642f9ad96cd435c28532aee433da3771d6e456
filipebfernandes <f92fernandes@gmail.com>
2018-12-07 17:39:49 +0000

    add copy

--- src/sections/ServicesHero/index.js
+++ src/sections/ServicesHero/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import Section from "root/components/Section";
+import Typography from "root/components/Typography";
 import BackgroundVideoText from "root/components/BackgroundVideoText";
+
+import "./index.css";
 
 const column = 114;
 const gutter = 28;
@@ -9,44 +12,70 @@ const sixColumns = 6 * column + 5 * gutter;
 const breakpointMobile = 768;
 const breakpointDesktop = 1268;
 
+const renderCopy = () => (
+  <div>
+    <div styleName="left">
+      <Typography variant="h2" color="dark-blue" fontFamily="meta-serif">
+        Working together to achieve your goals
+      </Typography>
+    </div>
+    <div styleName="right">
+      <Typography color="dark-blue">
+        We’ll work alongside you from the early stages of product research and
+        design to development and deployment, all while continuously delivering
+        you product updates.
+      </Typography>
+    </div>
+  </div>
+);
+
+const renderDesktop = () => (
+  <Section>
+    <BackgroundVideoText
+      key="desktop"
+      labels={["A dynamic team to design", "and develop your product"]}
+      poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/oceanshot.jpg"
+      video="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm"
+      height="195px"
+      width={`${eightColumns}px`}
+    />
+    {renderCopy()}
+  </Section>
+);
+
+const renderMobile = () => (
+  <Section>
+    <div>Implement Mobile</div>
+    {renderCopy()}
+  </Section>
+);
+
+const renderTablet = () => (
+  <Section>
+    <BackgroundVideoText
+      key="tablet"
+      labels={["A dynamic team to", "design and develop", "your product"]}
+      poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/oceanshot.jpg"
+      video="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm"
+      height="330px"
+      width={`${sixColumns}px`}
+    />
+    {renderCopy()}
+  </Section>
+);
+
 const ServicesHero = () => {
   const width = document.documentElement.clientWidth;
 
   if (width >= breakpointDesktop) {
-    return (
-      <Section>
-        <BackgroundVideoText
-          key="desktop"
-          labels={["A dynamic team to design", "and develop your product"]}
-          poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/oceanshot.jpg"
-          video="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm"
-          height="195px"
-          width={`${eightColumns}px`}
-        />
-      </Section>
-    );
+    return renderDesktop();
   }
 
   if (width < breakpointMobile) {
-    return (
-      <Section>
-        <div>Implement Mobile</div>
-      </Section>
-    );
+    return renderMobile();
   }
 
-  return (
-    <Section>
-      <BackgroundVideoText
-        key="tablet"
-        labels={["A dynamic team to", "design and develop", "your product"]}
-        poster="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/oceanshot.jpg"
-        video="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4273/ocean-small.webm"
-        height="330px"
-        width={`${sixColumns}px`}
-      />
-    </Section>
-  );
+  return renderTablet();
 };
 
 export default ServicesHero;
