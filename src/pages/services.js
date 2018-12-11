e0bbae81b82e0132bb37696eefb3f42e00f3540c
José Francisco Sousa <josesousa9000@gmail.com>
2018-12-11 14:44:23 +0000

    Add footer to services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Layout from "root/components/Layout";
 import Navbar from "root/components/Navbar";
+import Footer from "root/components/Footer";
 import ServicesHero from "root/sections/ServicesHero";
 
 import ServicesTechnologies from "root/sections/ServicesTechnologies";
@@ -15,6 +16,7 @@ const CompanyPage = () => (
     />
     <ServicesHero />
     <ServicesTechnologies />
+    <Footer />
   </Layout>
 );
 
