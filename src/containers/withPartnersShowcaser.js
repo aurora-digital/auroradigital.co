19dbe8f8a350a53d647cb43201e360b85549d986
José Sousa <josesousa9000@gmail.com>
2018-12-11 11:12:31 +0000

    Rotate partner cards automatically

--- src/containers/withPartnersShowcaser.js
+++ src/containers/withPartnersShowcaser.js
@@ -5,10 +5,38 @@ import PartnerCardMobile from "../components/PartnerCardMobile";
 export default showcasePartnersData => WrappedComponent => {
   class withPartnersShowcaser extends Component {
     state = {
+      currentIndex: 0,
       selectedPartner: showcasePartnersData[0],
     };
 
+    componentDidMount() {
+      this.interval = setInterval(() => {
+        const newCurrentIndex =
+          this.state.currentIndex === showcasePartnersData.length - 1
+            ? 0
+            : this.state.currentIndex + 1;
+        const newSelectedPartner = showcasePartnersData[newCurrentIndex];
+
+        this.setState({
+          currentIndex: newCurrentIndex,
+          selectedPartner: newSelectedPartner,
+        });
+      }, 7000);
+    }
+
+    componentWillUnmount() {
+      this.clearComponentInterval();
+    }
+
+    clearComponentInterval = () => {
+      clearInterval(this.interval);
+
+      this.interval = null;
+    };
+
     handleSelect = event => {
+      this.clearComponentInterval();
+
       const partnerName = event.currentTarget.name;
 
       if (!partnerName) return;
