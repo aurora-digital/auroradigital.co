715f53bafadb3711255990a248ae99a8841942cd
José Francisco Sousa <josesousa9000@gmail.com>
2018-12-20 11:18:21 +0000

    Add services "work" section
    Also create ListItem component, to have a line with an indicator icon

--- src/pages/services.js
+++ src/pages/services.js
@@ -4,14 +4,12 @@ import Navbar from "root/components/Navbar";
 import Footer from "root/components/Footer";
 import ServicesHero from "root/sections/ServicesHero";
 import ServicesWork from "root/sections/ServicesWork";
-import ServicesTechnologies from "root/sections/ServicesTechnologies";
 
 const ServicesPage = () => (
   <Layout>
     <Navbar currentPage="services" theme="primary" />
     <ServicesHero />
     <ServicesWork />
-    <ServicesTechnologies />
     <Footer />
   </Layout>
 );
