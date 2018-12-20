13a402f8c531808916dc36aa2f94653a15fe7a41
Tânia Rocha <taniapassosrocha@gmail.com>
2018-12-20 10:52:08 +0000

    Update home services

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -1,41 +1,89 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Link from "gatsby-link";
 
 import Typography from "root/components/Typography";
 import capitalize from "lodash/capitalize";
-import asterisk from "root/assets/images/asterisk.svg";
 
 import "./index.css";
 
-const Service = ({ serviceName, image, specificServices }) => (
-  <div styleName="services">
-    <div styleName="service-type">
-      <img styleName="asterisk" src={asterisk} alt="asterisk" />
-      <div styleName="type">
-        <Typography variant="h3" color="dark-blue">
-          {capitalize(serviceName)}
-        </Typography>
-      </div>
-    </div>
-    <div styleName="service-description">
-      <div>
-        <img src={image} alt={serviceName} />
-      </div>
-      <div styleName="description">
-        {specificServices.map(specificService => (
-          <Typography variant="body" color="dark-blue" key={specificService}>
-            {specificService}
-          </Typography>
-        ))}
-      </div>
-    </div>
-  </div>
-);
+export default class Service extends React.Component {
+  static propTypes = {
+    serviceName: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    specificServices: PropTypes.arrayOf(PropTypes.string).isRequired,
+  };
 
-Service.propTypes = {
-  serviceName: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  specificServices: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
+  componentDidMount = () => {
+    const { serviceName } = this.props;
+    const options = {
+      paths: `#circle-path-${serviceName}`,
+      color: "#5764F7",
+      intensity: 0.7,
+      pointsNumber: 8,
+      fastness: 1 / 40,
+      mouseIncidence: 20,
+      maxDistance: 40,
+    };
 
-export default Service;
+    const Jelly = require("jelly.js"); // eslint-disable-line
+
+    this[`jelly-${serviceName}`] = new Jelly(
+      `#jelly-canvas-${serviceName}`,
+      options,
+    );
+  };
+
+  render() {
+    const { serviceName, image, specificServices } = this.props;
+
+    return (
+      <div styleName="services">
+        <div styleName="link">
+          <Link to="/services" styleName="button">
+            <Typography
+              color="light-blue"
+              fontFamily="meta-serif"
+              weight="bold"
+            >
+              View services
+            </Typography>
+          </Link>
+        </div>
+        <div styleName="service-description">
+          <div styleName="shape-container">
+            <svg fill="none" xmlns="http://www.w3.org/2000/svg" styleName="svg">
+              <path
+                id={`circle-path-${serviceName}`}
+                d="M87.6203 3.86589C170.068 -22.4362 267.077 91.1204 184.67 175.467C140.801 220.37 69.7691 191.213 22.4752 148.651C-24.8187 106.089 5.17273 30.168 87.6203 3.86589Z"
+              />
+            </svg>
+            <div>
+              <canvas id={`jelly-canvas-${serviceName}`} />
+            </div>
+            <img styleName="image" src={image} alt={serviceName} />
+          </div>
+
+          <div styleName="description">
+            <div styleName="type">
+              <Typography variant="body" weight="bold" color="dark-blue">
+                {capitalize(serviceName)}
+              </Typography>
+            </div>
+            <div styleName="description-services">
+              {specificServices.map(specificService => (
+                <Typography
+                  variant="body"
+                  color="dark-blue"
+                  key={specificService}
+                >
+                  {specificService}
+                </Typography>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
