d580cccad51a0780a1d6e6b972513f2e0f9a5087
Tânia Rocha <taniapassosrocha@gmail.com>
2018-12-21 10:00:27 +0000

    Add site title and meta description

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,54 +1,30 @@
 import React, { Component } from "react";
-import { graphql, StaticQuery } from "gatsby";
 import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 
 import "./index.css";
 
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            keywords
-          }
-        }
-      }
-    `}
-    render={data => <Layout data={data} {...props} />}
-  />
-);
-
-class Layout extends Component {
+export default class Layout extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
-    data: PropTypes.shape({
-      site: PropTypes.shape({
-        siteMetadata: PropTypes.shape({
-          title: PropTypes.string.isRequired,
-          description: PropTypes.string.isRequired,
-          keywords: PropTypes.string.isRequired,
-        }).isRequired,
-      }).isRequired,
-    }).isRequired,
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    keywords: PropTypes.string,
+  };
+
+  static defaultProps = {
+    keywords: "",
   };
 
   render() {
-    const {
-      title: siteTitle,
-      description: siteDescription,
-      keywords: siteKeywords,
-    } = this.props.data.site.siteMetadata;
+    const { title, description, keywords } = this.props;
 
     return (
       <div>
         <Helmet>
-          <title>{siteTitle}</title>
-          <meta name="description" content={siteDescription} />
-          <meta name="keywords" content={siteKeywords} />
+          <title>{title}</title>
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Helmet>
         <div>{this.props.children}</div>
