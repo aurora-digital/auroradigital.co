0ad5b2cd9b4c6f06fe09c11d23ce795988252937
José Francisco Sousa <josesousa9000@gmail.com>
2018-12-21 11:48:42 +0000

    Fix jelly bubbles on mobile
    Hide canvas in mobile and show original SVG

--- src/components/Service/index.js
+++ src/components/Service/index.js
@@ -52,15 +52,22 @@ export default class Service extends React.Component {
         </div>
         <div styleName="service-description">
           <div styleName="shape-container">
-            <svg fill="none" xmlns="http://www.w3.org/2000/svg" styleName="svg">
+            <svg
+              height="220"
+              width="220"
+              viewBox="0 0 220 220"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              styleName="svg"
+            >
               <path
                 id={`circle-path-${serviceName}`}
                 d="M87.6203 3.86589C170.068 -22.4362 267.077 91.1204 184.67 175.467C140.801 220.37 69.7691 191.213 22.4752 148.651C-24.8187 106.089 5.17273 30.168 87.6203 3.86589Z"
               />
             </svg>
-            <div>
-              <canvas id={`jelly-canvas-${serviceName}`} />
-            </div>
+
+            <canvas id={`jelly-canvas-${serviceName}`} styleName="canvas" />
+
             <img styleName="image" src={image} alt={serviceName} />
           </div>
 
