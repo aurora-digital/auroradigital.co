c0da776f3eb01c7f26d0d64f5e39604181f1cbd4
José Sousa <josesousa9000@gmail.com>
2018-12-21 17:04:21 +0000

    Gatsby link fix

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import GatsbyLink from "gatsby-link";
+import { Link as GatsbyLink } from "gatsby";
 import classNames from "classnames/dedupe";
 
 import "./index.css";
