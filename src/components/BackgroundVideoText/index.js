d0929194287aa82029f430eab4438361925d1259
Tânia Rocha <taniapassosrocha@gmail.com>
2018-12-27 17:30:18 +0000

    Replace svg background by screen mix-blend-mode. We've been having a lot of problems with using svg as a text background to text. I replaced the svg backgrounds by the screen blend mode. To browsers without support to screen blend, the text have the lavender color.

--- src/components/BackgroundVideoText/index.js
+++ src/components/BackgroundVideoText/index.js
@@ -1,51 +1,75 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import map from "lodash/map";
+import withWindowDimensions from "root/containers/withWindowDimensions";
+
 import Typography from "../../components/Typography";
 
 import "./index.css";
 
-const fontHeight = 112;
+const breakpointMobile = 768;
 
+@withWindowDimensions
 export default class BackgroundVideoText extends Component {
   static propTypes = {
-    labels: PropTypes.arrayOf(PropTypes.string).isRequired,
+    width: PropTypes.number,
+    label: PropTypes.string.isRequired,
     video: PropTypes.string.isRequired,
     poster: PropTypes.string,
-    width: PropTypes.string.isRequired,
-    height: PropTypes.string.isRequired,
   };
 
   static defaultProps = {
     poster: "",
+    width: null,
+  };
+
+  state = {
+    videoHeight: null,
+    videoWidth: null,
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      videoHeight: `${this.title.clientHeight}px`,
+      videoWidth: `${this.title.clientWidth}px`,
+    });
+  };
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.width !== this.props.width || nextState !== this.state) {
+      if (nextProps.width !== this.props.width) {
+        this.setState({
+          videoHeight: `${this.title.clientHeight}px`,
+          videoWidth: `${this.title.clientWidth}px`,
+        });
+      }
+
+      return true;
+    }
+
+    return false;
+  }
+
+  handleRef = element => {
+    this.title = element;
   };
 
   render() {
-    const { labels, video, poster, width, height } = this.props;
+    const { width, label, poster, video } = this.props;
 
-    return (
-      <div styleName="root" style={{ width, height }}>
-        <Typography weight="bold" variant="h1" fontFamily="meta-serif">
-          <svg
-            width={width}
-            height={height}
-            version="1.1"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <defs>
-              <mask id="mask" x="0" y="0" width="100%" height="100%">
-                <rect x="0" y="0" width="100%" height="100%" />
-                <text x="0" y="0">
-                  {map(labels, (label, index) => (
-                    <tspan key={index} x="0" y={fontHeight * (index + 1)}>
-                      {label}
-                    </tspan>
-                  ))}
-                </text>
-              </mask>
-            </defs>
-            <rect width="100%" height="100%" />
-          </svg>
+    if (width >= breakpointMobile) {
+      return (
+        <div
+          styleName="root"
+          style={{
+            width: this.state.videoWidth,
+            height: this.state.videoHeight,
+          }}
+        >
+          <div styleName="title" ref={this.handleRef}>
+            <Typography weight="bold" variant="h1" fontFamily="meta-serif">
+              {label}
+            </Typography>
+          </div>
           <video
             src={video}
             poster={poster}
@@ -56,7 +80,23 @@ export default class BackgroundVideoText extends Component {
             preload="auto"
             type="video/mp4"
           />
-        </Typography>
+        </div>
+      );
+    }
+
+    return (
+      <div styleName="root">
+        <div
+          styleName="title"
+          style={{
+            backgroundImage: `url(${poster})`,
+          }}
+          ref={this.handleRef}
+        >
+          <Typography weight="bold" variant="h1" fontFamily="meta-serif">
+            {label}
+          </Typography>
+        </div>
       </div>
     );
   }
--- src/sections/ServicesTeam/index.js
+++ src/sections/ServicesTeam/index.js
@@ -3,19 +3,12 @@ import PropTypes from "prop-types";
 import Section from "root/components/Section";
 import Typography from "root/components/Typography";
 import BackgroundVideoText from "root/components/BackgroundVideoText";
-import BackgroundImageText from "root/components/BackgroundImageText";
-import ResponsiveRenderer from "root/components/ResponsiveRenderer";
 import Img from "gatsby-image";
 import video from "root/assets/videos/hero-services.mp4";
 import textBackground from "root/assets/images/hero-services.jpg";
 import withQuery from "./withQuery";
 
 import "./index.css";
-
-const column = 114;
-const gutter = 28;
-const eightColumns = 8 * column + 7 * gutter;
-const fiveColumns = 5 * column + 4 * gutter;
 
 class ServicesTeam extends Component {
   static propTypes = {
@@ -51,60 +44,17 @@ class ServicesTeam extends Component {
     );
   };
 
-  renderDesktop = () => (
-    <Section>
-      <BackgroundVideoText
-        key="desktop"
-        labels={["A dynamic team to design", "and develop your product"]}
-        poster={textBackground}
-        video={video}
-        height="240px"
-        width={`${eightColumns}px`}
-      />
-      {this.renderCopy()}
-    </Section>
-  );
-
-  renderMobile = () => (
-    <Section>
-      <BackgroundImageText image={textBackground}>
-        <Typography weight="bold" variant="h1" fontFamily="meta-serif">
-          &#8203;A dynamic team to design and develop your product
-        </Typography>
-      </BackgroundImageText>
-      {this.renderCopy()}
-    </Section>
-  );
-
-  renderTablet = () => (
-    <Section>
-      <BackgroundVideoText
-        key="tablet"
-        labels={["A dynamic", "team to design", "and develop", "your product"]}
-        poster={textBackground}
-        video={video}
-        height="470px"
-        width={`${fiveColumns}px`}
-      />
-      {this.renderCopy()}
-    </Section>
-  );
-
-  renderDefault = () => (
-    <div>
-      <noscript>{this.renderMobile()}</noscript>
-      {this.renderMobile()}
-    </div>
-  );
-
   render() {
     return (
-      <ResponsiveRenderer
-        renderDefault={this.renderDefault}
-        renderDesktop={this.renderDesktop}
-        renderTablet={this.renderTablet}
-        renderMobile={this.renderMobile}
-      />
+      <Section>
+        <BackgroundVideoText
+          label="Thriving for better healthcare solutions"
+          poster={textBackground}
+          video={video}
+          alt="services team"
+        />
+        {this.renderCopy()}
+      </Section>
     );
   }
 }
