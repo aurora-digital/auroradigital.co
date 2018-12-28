5b0bff2cbe094e50f45a77ff853bc9cbef5d5938
Tânia Rocha <taniapassosrocha@gmail.com>
2018-12-28 14:53:15 +0000

    Add particles animation to services

---
+++ src/components/Particles/images.js
@@ -0,0 +1,73 @@
+import ellipse from "root/assets/images/particles/Ellipse.svg";
+import ellipse2 from "root/assets/images/particles/Ellipse2.svg";
+import vector from "root/assets/images/particles/Vector.svg";
+import vector2 from "root/assets/images/particles/Vector2.svg";
+import vector3 from "root/assets/images/particles/Vector3.svg";
+
+export default [
+  {
+    src: ellipse,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse2,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: vector,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse2,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse2,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: vector,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: vector2,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse2,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: vector3,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse,
+    width: 200,
+    height: 200,
+  },
+  {
+    src: ellipse2,
+    width: 200,
+    height: 200,
+  },
+];
