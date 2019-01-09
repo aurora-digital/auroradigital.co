d2270215bd03866477278707db6ae35ca4e7d887
José Sousa <josesousa9000@gmail.com>
2019-01-09 15:14:45 +0000

    Add hover to contact link

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,20 +1,33 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import Link from "root/components/Link";
 
 import "./index.css";
 
 export default class Contact extends Component {
   static propTypes = {
+    underlineColor: PropTypes.oneOf(["light-blue", "dark-blue", "white"]),
+    hover: PropTypes.bool,
     children: PropTypes.node.isRequired,
   };
 
+  static defaultProps = {
+    underlineColor: "white",
+    hover: false,
+  };
+
   render() {
-    const { children } = this.props;
+    const { underlineColor, hover, children } = this.props;
 
     return (
-      <a styleName="root" href="mailto:contact@svhealth.io">
+      <Link
+        internal={false}
+        underlineColor={underlineColor}
+        hover={hover}
+        to="mailto:contact@svhealth.io"
+      >
         {children}
-      </a>
+      </Link>
     );
   }
 }
