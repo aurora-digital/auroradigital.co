e4cec42673e80c80e8fad29b4af269a571ac3315
José Francisco Sousa <josesousa9000@gmail.com>
2019-02-22 10:18:16 +0000

    Replace withWindowDimensions HOC with a useWindowDimensions hook
    Check hooks API for more info! I tried to replace this HOC with a Hook as it was a popular one that people were re-implementing in their projects.
    
    This is merely exploratory, no need to rush and approve this but at least you can see it in action.
    
    As for hooks vs classes debate, I have no opinion yet. Only thing I can say is that long term hooks can be less "bug-prone" as they don't deal with lifecycle events and are a more functional approach to side effects. They encapsulate effects nicely and I think it's something we should strive to start using more, as the React team is heading towards a more function-oriented approach instead of using classes.

--- src/components/BackgroundVideoText/index.js
+++ src/components/BackgroundVideoText/index.js
@@ -1,107 +1,73 @@
-import React, { Component } from "react";
+import React, { useRef, useState, useLayoutEffect } from "react";
 import PropTypes from "prop-types";
-import withWindowDimensions from "root/containers/withWindowDimensions";
-
-import Typography from "../Typography";
+import useWindowDimensions from "root/hooks/useWindowDimensions";
+import Typography from "root/components/Typography";
 
 import "./index.css";
 
 const breakpointMobile = 768;
 
-@withWindowDimensions
-export default class BackgroundVideoText extends Component {
-  static propTypes = {
-    width: PropTypes.number,
-    label: PropTypes.string.isRequired,
-    video: PropTypes.string.isRequired,
-    poster: PropTypes.string,
-  };
+function BackgroundVideoText(props) {
+  const { label, poster, video } = props;
 
-  static defaultProps = {
-    poster: "",
-    width: null,
-  };
+  const [videoSize, setVideoSize] = useState(null);
+  const { width } = useWindowDimensions();
+  const textRef = useRef(null);
 
-  state = {
-    videoHeight: null,
-    videoWidth: null,
-  };
-
-  componentDidMount = () => {
-    this.setState({
-      videoHeight: `${this.title.clientHeight}px`,
-      videoWidth: `${this.title.clientWidth}px`,
+  useLayoutEffect(() => {
+    setVideoSize({
+      width: `${textRef.current.clientWidth}px`,
+      height: `${textRef.current.clientHeight}px`,
     });
-  };
+  }, []);
 
-  shouldComponentUpdate(nextProps, nextState) {
-    const { width: oldWidth } = this.props;
-    const { width: newWidth } = nextProps;
-
-    if (newWidth !== oldWidth || nextState !== this.state) {
-      if (newWidth !== oldWidth) {
-        this.setState({
-          videoHeight: `${this.title.clientHeight}px`,
-          videoWidth: `${this.title.clientWidth}px`,
-        });
-      }
-
-      return true;
-    }
-
-    return false;
-  }
-
-  handleRef = element => {
-    this.title = element;
-  };
-
-  render() {
-    const { width, label, poster, video } = this.props;
-    const { videoWidth, videoHeight } = this.state;
-
-    if (width >= breakpointMobile) {
-      return (
-        <div
-          styleName="root"
-          style={{
-            width: videoWidth,
-            height: videoHeight,
-          }}
-        >
-          <div styleName="title" ref={this.handleRef}>
-            <Typography weight="bold" variant="h1" fontFamily="meta-serif">
-              {label}
-            </Typography>
-          </div>
-          <video
-            src={video}
-            poster={poster}
-            muted
-            autoPlay
-            loop
-            playsInline
-            preload="auto"
-            type="video/mp4"
-          />
-        </div>
-      );
-    }
-
+  if (width >= breakpointMobile) {
     return (
-      <div styleName="root">
-        <div
-          styleName="title"
-          style={{
-            backgroundImage: `url(${poster})`,
-          }}
-          ref={this.handleRef}
-        >
+      <div styleName="root" style={videoSize}>
+        <div styleName="title" ref={textRef}>
           <Typography weight="bold" variant="h1" fontFamily="meta-serif">
             {label}
           </Typography>
         </div>
+        <video
+          src={video}
+          poster={poster}
+          muted
+          autoPlay
+          loop
+          playsInline
+          preload="auto"
+          type="video/mp4"
+        />
       </div>
     );
   }
+
+  return (
+    <div styleName="root">
+      <div
+        styleName="title"
+        style={{
+          backgroundImage: `url(${poster})`,
+        }}
+        ref={textRef}
+      >
+        <Typography weight="bold" variant="h1" fontFamily="meta-serif">
+          {label}
+        </Typography>
+      </div>
+    </div>
+  );
 }
+
+BackgroundVideoText.propTypes = {
+  label: PropTypes.string.isRequired,
+  video: PropTypes.string.isRequired,
+  poster: PropTypes.string,
+};
+
+BackgroundVideoText.defaultProps = {
+  poster: "",
+};
+
+export default BackgroundVideoText;
--- src/components/Particles/index.js
+++ src/components/Particles/index.js
@@ -1,89 +1,76 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import Particles from "react-particles-js";
-import withWindowDimensions from "root/containers/withWindowDimensions";
+import React from "react";
+import ReactParticles from "react-particles-js";
+import useWindowDimensions from "root/hooks/useWindowDimensions";
 import images from "./images";
 
 const breakpointMobile = 768;
 const breakpointDesktop = 1268;
 
-@withWindowDimensions
-class ParticlesClass extends Component {
-  static propTypes = {
-    width: PropTypes.number,
-  };
+export default function Particles() {
+  const { width } = useWindowDimensions();
 
-  static defaultProps = {
-    width: null,
-  };
+  let particlesHeight = 1150;
 
-  render() {
-    const { width } = this.props;
+  if (width < breakpointDesktop) {
+    particlesHeight = 1000;
+  }
 
-    let particlesHeight = 1150;
+  if (width < breakpointMobile) {
+    return null;
+  }
 
-    if (width < breakpointDesktop) {
-      particlesHeight = 1000;
-    }
-
-    if (width < breakpointMobile) {
-      return null;
-    }
-
-    return (
-      <Particles
-        width="100%"
-        height={particlesHeight}
-        params={{
-          particles: {
-            number: {
-              value: images.length || 0,
-              density: {
-                enable: false,
-                value_area: 800,
-              },
-            },
-            line_linked: {
+  return (
+    <ReactParticles
+      width="100%"
+      height={particlesHeight}
+      params={{
+        particles: {
+          number: {
+            value: images.length || 0,
+            density: {
               enable: false,
-            },
-            move: {
-              speed: 1,
-              out_mode: "out",
-              random: true,
-              attract: {
-                enable: true,
-              },
-            },
-            shape: {
-              type: ["images"],
-              images,
-            },
-            color: {
-              value: "#9C447F",
-            },
-            opacity: {
-              value: 1,
-              anim: {
-                enable: false,
-                speed: 3,
-                opacity_min: 0.5,
-              },
-            },
-            size: {
-              value: 70,
-              random: false,
-              anim: {
-                enable: false,
-                speed: 4,
-                size_min: 100,
-                sync: false,
-              },
+              value_area: 800,
             },
           },
-          retina_detect: false,
-        }}
-      />
-    );
-  }
+          line_linked: {
+            enable: false,
+          },
+          move: {
+            speed: 1,
+            out_mode: "out",
+            random: true,
+            attract: {
+              enable: true,
+            },
+          },
+          shape: {
+            type: ["images"],
+            images,
+          },
+          color: {
+            value: "#9C447F",
+          },
+          opacity: {
+            value: 1,
+            anim: {
+              enable: false,
+              speed: 3,
+              opacity_min: 0.5,
+            },
+          },
+          size: {
+            value: 70,
+            random: false,
+            anim: {
+              enable: false,
+              speed: 4,
+              size_min: 100,
+              sync: false,
+            },
+          },
+        },
+        retina_detect: false,
+      }}
+    />
+  );
 }
-export default ParticlesClass;
