6be40e15e6cfd86c7a4c69e790c527b194ee80ce
Tânia Rocha <taniapassosrocha@gmail.com>
2019-06-03 10:40:25 +0100

    Add company page

---
+++ src/sections/CompanyHero/index.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Parallax } from "react-scroll-parallax";
+import Section from "root/components/Section";
+import Navbar from "root/components/Navbar";
+import Typography from "root/components/Typography";
+
+import "./index.css";
+
+const CompanyHero = () => (
+  <div styleName="root">
+    <Navbar theme="primary" />
+    <Section verticalSpacing={false}>
+      <div styleName="title">
+        <div styleName="parallax-container">
+          <Parallax styleName="parallax" x={[-78, 20]} tagOuter="figure">
+            <Typography color="oxford-blue" weight="bold" variant="h1">
+              Thriving for
+            </Typography>
+          </Parallax>
+
+          <Parallax styleName="parallax" x={[30, -30]} tagOuter="figure">
+            <Typography color="oxford-blue" weight="bold" variant="h1">
+              better healthcare
+            </Typography>
+          </Parallax>
+        </div>
+
+        <div styleName="parallax-fallback">
+          <Typography color="oxford-blue" weight="bold" variant="h1">
+            Thriving for better healthcare
+          </Typography>
+        </div>
+      </div>
+      <div styleName="copy">
+        <Typography color="oxford-blue" variant="h3" weight="medium">
+          The tech revolution came to change the world, but digital healthcare
+          it’s still in its infancy
+        </Typography>
+      </div>
+    </Section>
+  </div>
+);
+
+export default CompanyHero;
