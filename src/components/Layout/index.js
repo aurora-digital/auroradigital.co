b7b1b41be72e62eee45d195569758a1ba272aab7
José Francisco Sousa <josesousa9000@gmail.com>
2019-06-03 10:48:10 +0100

    Fix viewport metatag and fix small bugs
    * Fix svg viewbox
    * Improve parallax alignment

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -26,7 +26,6 @@ export default class Layout extends Component {
           <title>{title}</title>
           <meta name="description" content={description} />
           <meta name="keywords" content={keywords} />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Helmet>
         <ParallaxProvider>
           <div>{children}</div>
