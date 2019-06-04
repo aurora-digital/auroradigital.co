86afa43657f5ab427541f166eda1c80e7e3bc207
José Sousa <josesousa9000@gmail.com>
2019-06-04 16:01:53 +0100

    Remove unused css

--- src/sections/HomeMission/index.js
+++ src/sections/HomeMission/index.js
@@ -9,39 +9,37 @@ import "./index.css";
 export default function HomeMission() {
   return (
     <Section verticalSpacing={false}>
-      <div styleName="root">
-        <div styleName="title">
-          <div styleName="parallax-container-wrapper">
-            <div styleName="parallax-container">
-              <Parallax styleName="parallax" x={[-20, 40]} tagOuter="figure">
-                <Typography color="klein-blue" weight="bold" variant="h1">
-                  Creating the best
-                </Typography>
-              </Parallax>
+      <div styleName="title">
+        <div styleName="parallax-container-wrapper">
+          <div styleName="parallax-container">
+            <Parallax styleName="parallax" x={[-20, 40]} tagOuter="figure">
+              <Typography color="klein-blue" weight="bold" variant="h1">
+                Creating the best
+              </Typography>
+            </Parallax>
 
-              <Parallax styleName="parallax" x={[20, -65]} tagOuter="figure">
-                <Typography color="klein-blue" weight="bold" variant="h1">
-                  possible experience
-                </Typography>
-              </Parallax>
-            </div>
+            <Parallax styleName="parallax" x={[20, -65]} tagOuter="figure">
+              <Typography color="klein-blue" weight="bold" variant="h1">
+                possible experience
+              </Typography>
+            </Parallax>
           </div>
         </div>
+      </div>
 
-        <div styleName="parallax-fallback">
-          <Typography color="klein-blue" weight="bold" variant="h1">
-            Creating the best possible experience
-          </Typography>
-        </div>
+      <div styleName="parallax-fallback">
+        <Typography color="klein-blue" weight="bold" variant="h1">
+          Creating the best possible experience
+        </Typography>
+      </div>
 
-        <div styleName="copy">
-          <Typography color="oxford-blue" variant="h3">
-            Healthcare solutions are quite sensitive and need to be built with
-            100% focus on the user’s needs, safety and privacy. We work
-            alongside our customers, from research to user-testing, to build
-            excellent software.
-          </Typography>
-        </div>
+      <div styleName="copy">
+        <Typography color="oxford-blue" variant="h3">
+          Healthcare solutions are quite sensitive and need to be built with
+          100% focus on the user’s needs, safety and privacy. We work alongside
+          our customers, from research to user-testing, to build excellent
+          software.
+        </Typography>
       </div>
     </Section>
   );
