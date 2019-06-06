175fe724a09da728c88053beadfd76f774d50953
fbfernandes <f92fernandes@gmail.com>
2019-06-06 15:42:21 +0100

    Company Advisors copy

---
+++ src/sections/CompanyAdvisorsCopy/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typography from "root/components/Typography";
+import Section from "root/components/Section";
+
+import "./index.css";
+
+export default function CompanyAdvisorsCopy() {
+  return (
+    <Section verticalSpacing={false}>
+      <div styleName="root">
+        <div styleName="title">
+          <Typography variant="h3" color="oxford-blue" weight="medium">
+            We are backed
+          </Typography>
+          <Typography variant="h3" color="oxford-blue" weight="medium">
+            by an excellent advisory board
+          </Typography>
+        </div>
+
+        <Typography color="oxford-blue">
+          Our advisors have a vast experience in launching multiple large-scale
+          technological products in both the Medtech and Fintech areas - their
+          direct insight will empower us to deliver you an even better
+          experience.
+        </Typography>
+      </div>
+    </Section>
+  );
+}
