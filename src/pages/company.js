d443b52c6ece847d807336842da3a2181d958508
fbfernandes <f92fernandes@gmail.com>
2019-06-06 11:52:08 +0100

    Company core values

--- src/pages/company.js
+++ src/pages/company.js
@@ -4,6 +4,7 @@ import Footer from "root/components/Footer";
 import CompanyHero from "../sections/CompanyHero";
 import CompanyMedtechRevolution from "../sections/CompanyMedtechRevolution";
 import CompanyTeam from "../sections/CompanyTeam";
+import CompanyValues from "../sections/CompanyValues";
 
 export default class CompanyPage extends Component {
   render() {
@@ -14,7 +15,7 @@ export default class CompanyPage extends Component {
       >
         <CompanyHero />
         <CompanyMedtechRevolution />
-
+        <CompanyValues />
         <CompanyTeam />
 
         <Footer />
