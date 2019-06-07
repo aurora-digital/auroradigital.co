5c3310c8581964f1a132d141ae970ce8675dcb7f
Tânia Rocha <taniapassosrocha@gmail.com>
2019-06-07 14:58:07 +0100

    Fix company hero alignment (mobile)

--- src/sections/CompanyHero/index.js
+++ src/sections/CompanyHero/index.js
@@ -15,24 +15,31 @@ const CompanyHero = () => (
           styleName="parallax-container"
           aria-label="Striving for better healthcare"
         >
-          <div aria-hidden="true">
-            <Parallax styleName="parallax" x={[-78, 20]} tagOuter="figure">
-              <Typography color="oxford-blue" weight="bold" variant="h1">
-                Striving for
-              </Typography>
-            </Parallax>
+          <Parallax styleName="parallax" x={[-78, 20]} tagOuter="figure">
+            <Typography color="oxford-blue" weight="bold" variant="h1">
+              Striving for
+            </Typography>
+          </Parallax>
 
-            <Parallax styleName="parallax" x={[30, -30]} tagOuter="figure">
-              <Typography color="oxford-blue" weight="bold" variant="h1">
-                better healthcare
-              </Typography>
-            </Parallax>
-          </div>
+          <Parallax styleName="parallax" x={[30, -30]} tagOuter="figure">
+            <Typography color="oxford-blue" weight="bold" variant="h1">
+              better healthcare
+            </Typography>
+          </Parallax>
         </div>
 
-        <div styleName="parallax-fallback">
+        <div
+          styleName="parallax-fallback"
+          aria-label="Striving for better healthcare"
+        >
           <Typography color="oxford-blue" weight="bold" variant="h1">
             Striving for better healthcare
+          </Typography>
+          <Typography color="oxford-blue" weight="bold" variant="h1">
+            better
+          </Typography>
+          <Typography color="oxford-blue" weight="bold" variant="h1">
+            healthcare
           </Typography>
         </div>
       </div>
