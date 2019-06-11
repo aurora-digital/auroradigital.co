6de64e893d01ee49a24e4fe17536d7fc1254a457
José Sousa <josesousa9000@gmail.com>
2019-06-11 12:29:02 +0100

    Change text sizes on home mission section

--- src/sections/HomeMission/index.js
+++ src/sections/HomeMission/index.js
@@ -13,13 +13,13 @@ export default function HomeMission() {
         <div styleName="parallax-container-wrapper">
           <div styleName="parallax-container">
             <Parallax styleName="parallax" x={[-20, 40]} tagOuter="figure">
-              <Typography color="klein-blue" weight="bold" variant="h1">
+              <Typography color="klein-blue" weight="bold" variant="h2">
                 Creating the best
               </Typography>
             </Parallax>
 
             <Parallax styleName="parallax" x={[20, -65]} tagOuter="figure">
-              <Typography color="klein-blue" weight="bold" variant="h1">
+              <Typography color="klein-blue" weight="bold" variant="h2">
                 possible experiences
               </Typography>
             </Parallax>
@@ -27,14 +27,14 @@ export default function HomeMission() {
         </div>
 
         <div styleName="parallax-fallback">
-          <Typography color="klein-blue" weight="bold" variant="h1">
+          <Typography color="klein-blue" weight="bold" variant="h2">
             Creating the best possible experiences
           </Typography>
         </div>
       </div>
 
       <div styleName="copy">
-        <Typography color="oxford-blue" variant="h3">
+        <Typography color="oxford-blue">
           Healthcare solutions are quite sensitive and need to be built with
           100% focus on the user’s needs, safety and privacy. We work alongside
           our customers, from research to user-testing, to build excellent
