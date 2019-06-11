347bc48a8e9a949ac7e9bc8d4b34ade8639b9d20
filipebfernandes <f92fernandes@gmail.com>
2019-06-11 17:39:09 +0100

    add address tag

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -25,7 +25,7 @@ const Arrow = () => (
 );
 
 const renderAddress = (name, address) => (
-  <div styleName="titled-links">
+  <address styleName="titled-links">
     <Typography variant="small-body" color="oxford-blue" weight="bold">
       {name}
     </Typography>
@@ -35,7 +35,7 @@ const renderAddress = (name, address) => (
       </Typography>
       <Arrow />
     </a>
-  </div>
+  </address>
 );
 
 const SOCIAL_LINKS = [
