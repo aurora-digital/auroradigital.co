c112894dd22752174802b75417ed3b808024f46d
filipebfernandes <f92fernandes@gmail.com>
2019-06-13 09:43:11 +0100

    wip

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -23,7 +23,7 @@ const COLOR_THEME_SECONDARY = {
 
 export default class Navbar extends Component {
   static propTypes = {
-    currentPage: PropTypes.oneOf(["home", "company", "blop"]),
+    currentPage: PropTypes.oneOf(["home", "company", "blog"]),
     theme: PropTypes.oneOf(["primary", "secondary"]),
   };
 
