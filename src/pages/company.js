b6d18c9b8c13061eed096231758f3f39c430c7cb
José Sousa <josesousa9000@gmail.com>
2019-06-13 23:47:01 +0100

    Replace thriving with striving

--- src/pages/company.js
+++ src/pages/company.js
@@ -13,7 +13,7 @@ export default class CompanyPage extends Component {
   render() {
     return (
       <Layout
-        title="Thriving for better healthcare solutions | Aurora"
+        title="Striving for better healthcare solutions | Aurora"
         description="We want software accessible and readily available to everyone in the world, improve people's lives and guiding them to a healthier and more sustainable lifestyle."
       >
         <CompanyHero />
