cdad9eb094eb9488d0eaf11b70176903d0fa0d0e
José Sousa <josesousa9000@gmail.com>
2019-06-27 11:43:52 +0100

    Fix

--- src/components/FadeUpOnScroll/index.js
+++ src/components/FadeUpOnScroll/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { useInView } from "react-intersection-observer";
@@ -6,20 +6,21 @@ import { useInView } from "react-intersection-observer";
 import "./index.css";
 
 const FadeUpOnScroll = ({ children, className }) => {
-  const [animated, setAnimated] = useState(false);
+  const [readyToAnimate, setReadyToAnimate] = useState(false);
   const [ref, inView] = useInView();
-  const handleAnimationEnd = () => setAnimated(true);
   const styles = classNames("root", {
-    animate: inView && !animated,
+    animate: readyToAnimate,
   });
 
+  useEffect(
+    () => {
+      if (inView) setReadyToAnimate(true);
+    },
+    [inView],
+  );
+
   return (
-    <div
-      className={className}
-      ref={ref}
-      styleName={styles}
-      onAnimationEnd={handleAnimationEnd}
-    >
+    <div className={className} ref={ref} styleName={styles}>
       {children}
     </div>
   );
