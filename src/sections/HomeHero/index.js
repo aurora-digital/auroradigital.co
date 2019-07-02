f0d6be9940f4e48c0b47422e9406d8e02e62bfca
José Francisco Sousa <josesousa9000@gmail.com>
2019-07-02 15:18:26 +0100

    Add custom parallax animation to home hero section
    Uses react scroll magic to achieve the wanted effect.
    
    Also fix the sliding shutter effect to occupy the whole viewport.

--- src/sections/HomeHero/index.js
+++ src/sections/HomeHero/index.js
@@ -1,13 +1,22 @@
 import React from "react";
 
 import HomeHeroNoScroll from "./NoScroll";
+import HomeHeroWithScroll from "./WithScroll";
 
 import "./index.css";
 
 function HomeHero() {
   return (
     <>
-      <HomeHeroNoScroll />
+      <div styleName="shutter" />
+
+      <div styleName="no-scroll">
+        <HomeHeroNoScroll />
+      </div>
+
+      <div styleName="with-scroll">
+        <HomeHeroWithScroll />
+      </div>
     </>
   );
 }
