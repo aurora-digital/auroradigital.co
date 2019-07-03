4775ab177d0a80ee387451c1b01581cd0c907f45
José Francisco Sousa <josesousa9000@gmail.com>
2019-07-03 15:33:51 +0100

    Make the website noscript friendly
    Change a few things just to make the website usable without javascript.
    
    Most changes are on the FadeUpScroll component, using a noscript tag to bypass the animation.

--- src/components/FadeUpOnScroll/index.js
+++ src/components/FadeUpOnScroll/index.js
@@ -4,22 +4,33 @@ import classNames from "classnames";
 import { useInView } from "react-intersection-observer";
 
 import "./index.css";
+import useDetectJavascript from "root/hooks/useDetectJavascript";
 
 const FadeUpOnScroll = ({ children, className }) => {
+  const hasJavascript = useDetectJavascript();
   const [readyToAnimate, setReadyToAnimate] = useState(false);
   const [ref, inView] = useInView();
   const styles = classNames("root", {
     animate: readyToAnimate,
+    hide: !hasJavascript,
   });
 
-  useEffect(() => {
-    if (inView) setReadyToAnimate(true);
-  }, [inView]);
+  useEffect(
+    () => {
+      if (inView) setReadyToAnimate(true);
+    },
+    [inView],
+  );
 
   return (
-    <div className={className} ref={ref} styleName={styles}>
-      {children}
-    </div>
+    <>
+      <noscript>
+        <div styleName="noscript">{children}</div>
+      </noscript>
+      <div className={className} ref={ref} styleName={styles}>
+        {children}
+      </div>
+    </>
   );
 };
 
