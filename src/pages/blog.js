17f38b6db16ba166c16556686287ee1f310ae453
filipebfernandes <f92fernandes@gmail.com>
2019-07-08 18:37:42 +0100

    wip

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,11 +3,11 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 
 import Layout from "root/components/Layout";
-import Typography from "root/components/Typography";
+
 import Navbar from "root/components/Navbar";
 import Section from "root/components/Section";
 import Footer from "root/components/Footer";
-// import BlogPostPreview from "root/components/BlogPostPreview";
+import BlogPostPreview from "root/components/BlogPostPreview";
 import FeaturedBlogPostPreview from "root/components/FeaturedBlogPostPreview";
 
 import "./blog.css";
@@ -44,61 +44,37 @@ function Blog({
 }) {
   const getAuthorInfo = node =>
     // eslint-disable-next-line
-     require(`../../blog/authors/${node.frontmatter.author}.json`)
-  ;
+    require(`../../blog/authors/${node.frontmatter.author}.json`);
 
   return (
     <Layout title="Blog | Aurora" description="Aurora's Blog">
       <Navbar currentPage="blog" theme="primary" />
       <Section verticalSpacing={false}>
         <div styleName="root">
-          <div
-            key={edges[0].node.frontmatter.path}
-            styleName="featured-blog-preview"
-          >
-            <FeaturedBlogPostPreview
-              {...edges[0].node.frontmatter}
-              author={getAuthorInfo(edges[0].node)}
-            />
-          </div>
+          {edges.map(({ node }, index) => {
+            // eslint-disable-next-line
+            const author = require(`../../blog/authors/${node.frontmatter.author}.json`);
 
-          <div styleName="more-coming-soon">
-            <Typography variant="h3" weight="medium" color="oxford-blue">
-              More words coming soon
-            </Typography>
-          </div>
-          {/* UNCOMMENT TO RENDER ALL POSTS
+            if (index === 0) {
+              return (
+                <div
+                  key={node.frontmatter.path}
+                  styleName="featured-blog-preview"
+                >
+                  <FeaturedBlogPostPreview
+                    {...node.frontmatter}
+                    author={author}
+                  />
+                </div>
+              );
+            }
 
-          <div styleName="root">
-        {edges.map(({ node }, index) => {
-          // eslint-disable-next-line
-          const author = require(`../../blog/authors/${
-            node.frontmatter.author
-          }.json`);
-
-          if (index === 0) {
             return (
-              <div
-                key={node.frontmatter.path}
-                styleName="featured-blog-preview"
-              >
-                <FeaturedBlogPostPreview
-                  {...node.frontmatter}
-                  author={author}
-                />
+              <div key={node.frontmatter.path} styleName="blog-preview">
+                <BlogPostPreview {...node.frontmatter} author={author} />
               </div>
             );
-          }
-
-          return (
-            <div key={node.frontmatter.path} styleName="blog-preview">
-              <BlogPostPreview {...node.frontmatter} author={author} />
-            </div>
-          );
-        })}
-      </div>
-
-      */}
+          })}
         </div>
       </Section>
       <Footer />
