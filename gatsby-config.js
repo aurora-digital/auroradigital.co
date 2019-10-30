440d52d01d822428f6a1723ebc14527da53d37f7
José Sousa <josesousa9000@gmail.com>
2019-10-30 18:09:02 +0000

    Noopener

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
             resolve: "gatsby-remark-external-links",
             options: {
               target: "_blank",
-              rel: "nofollow",
+              rel: "noopener",
             },
           },
           "gatsby-remark-relative-images",
