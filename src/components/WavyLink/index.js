b334fc5e6309b5979ae5dd947a1dd73292e91bd2
Francisco Sousa <josesousa9000@gmail.com>
2020-02-25 12:04:13 +0000

    Replace hero animation and wavy links with framer-motion
    Use `framer-motion` (which is the best) to make scroll based animations, removing `react-scrollmagic` and `tornis` in the process.
    
    The new animations should be smoother on slower devices. Exchanging two packages by one should reduce bundle size and make everything slightly faster.

--- src/components/WavyLink/index.js
+++ src/components/WavyLink/index.js
@@ -2,21 +2,20 @@ import React from "react";
 import PropTypes from "prop-types";
 import classNames from "classnames";
 import { useInView } from "react-intersection-observer";
-import { get } from "lodash";
 
-import useScrollDirection from "root/hooks/useScrollDirection";
+import useScrollingDetector from "./useScrollingDetector";
 
 import "./index.css";
 
 function WavyLink({ url, children, label, animate }) {
   const [ref, inView] = useInView();
-  const scrollDirection = useScrollDirection();
+  const isScrolling = useScrollingDetector();
 
   const waveStyles = classNames("wave", {
-    moving: get(scrollDirection, "velocity.y") !== 0 && inView && animate,
+    moving: isScrolling && inView && animate,
   });
   const waveDarkStyles = classNames("wave darker", {
-    moving: get(scrollDirection, "velocity.y") !== 0 && inView && animate,
+    moving: isScrolling && inView && animate,
   });
 
   return (
