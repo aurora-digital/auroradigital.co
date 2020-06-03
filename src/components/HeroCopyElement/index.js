83ce5e7c5e239d0706482611141d65282ba8c732
Anonirato <vianajose.97@gmail.com>
2020-06-03 14:56:05 +0100

    Hero copy message animation.
    Why:
    
     - Necessary to implement bottom-up and up-bottom slide animation for hero copy message.
    
    How:
    
     - A timer to control which text to display and when to start a new animation.
    
     - Used `Framer Motion` for animations.

--- src/components/HeroCopyElement/index.js
+++ src/components/HeroCopyElement/index.js
@@ -1,76 +1,218 @@
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import Typography from "root/components/Typography";
+import updateDimensions from "root/hooks/useWindowDimensions";
 
 import "./index.css";
+
+const breakpointMobile = 800;
 
 function HeroCopyElement() {
   const [seconds, setSeconds] = useState(0);
   const [position, setPosition] = useState(0);
+  const dimensions = updateDimensions();
   const textPlaceholder = [
     { message: "For founders", title: "We materialize ideas" },
     { message: "For companies", title: "We are advisors" },
     { message: "For everyone", title: "We scale solutions" },
   ];
+  const transitionMessage = {
+    duration: 4.1,
+    ease: "circOut",
+    times: [0, 0.2, 0.75, 0.9, 1],
+    delay: 0.2,
+  };
+  const transitionTitle = {
+    yoyo: 1,
+    duration: 0.5,
+    ease: "easeInOut",
+    repeatDelay: 1.2,
+    delay: 1,
+  };
+  const transitionTitleMobile = {
+    yoyo: 1,
+    duration: 0.5,
+    ease: "easeInOut",
+    repeatDelay: 1.75,
+    delay: 1,
+  };
+  const transitionLine = {
+    duration: 0.75,
+    ease: "linear",
+    times: [0, 1],
+  };
+  const transitionLineMobile = {
+    duration: 0.75,
+    ease: "easeIn",
+    times: [0, 1],
+  };
+  const initialTitle = { y: "84px" };
 
   function getClassName(place) {
-    if (place === position) {
-      return "line-klein-blue";
-    }
-
-    return "line-baby-blue";
+    return position === place ? "line-klein-blue" : "line-baby-blue";
   }
 
-  function reset(value) {
-    setSeconds(value);
-    setPosition(value);
+  function display(place) {
+    return position === place ? "block" : "none";
+  }
+
+  function animateMessage(place) {
+    if (dimensions.width > breakpointMobile) {
+      return position === place
+        ? { x: [-117, 0, 0, 310, 310], opacity: [0, 1, 1, 0, 0] }
+        : "";
+    }
+
+    return position === place
+      ? {
+          x: [-117, 0, 0, 0, 0],
+          y: [0, 0, 0, 75, 75],
+          opacity: [0, 1, 1, 1, 1],
+        }
+      : "";
+  }
+
+  function animateTitle(place) {
+    return position === place ? { y: 0 } : "";
+  }
+
+  function reset(place) {
+    setSeconds(place);
+    setPosition(place);
   }
 
   useEffect(() => {
     let interval = null;
 
     interval = setInterval(() => {
-      setSeconds(secs => secs + 1);
-      setPosition((seconds + 1) % 3);
-    }, 2500);
+      const newSeconds = seconds + 1;
+
+      setSeconds(newSeconds);
+      setPosition(newSeconds % 3);
+    }, 4000);
 
     return () => clearInterval(interval);
-  }, [seconds]);
+  }, [seconds, position]);
 
-  return (
-    <div styleName="copy-section">
-      <div styleName="lines">
-        <button
-          type="button"
-          aria-label="clickable info 1"
-          styleName={getClassName(0)}
-          style={{ "--top": "0" }}
-          onClick={() => reset(0)}
-        />
-        <button
-          type="button"
-          aria-label="clickable info 2"
-          styleName={getClassName(1)}
-          style={{ "--top": "1" }}
-          onClick={() => reset(1)}
-        />
-        <button
-          type="button"
-          aria-label="clickable info 3"
-          styleName={getClassName(2)}
-          style={{ "--top": "2" }}
-          onClick={() => reset(2)}
-        />
-      </div>
-      <div styleName="message">
-        <Typography color="oxford-blue">
-          {textPlaceholder[position].message}
-        </Typography>
-        <Typography color="klein-blue" weight="bold" variant="h2">
-          {textPlaceholder[position].title}
-        </Typography>
-      </div>
-    </div>
-  );
+  function renderLine(transition, place) {
+    let animateLine;
+
+    if (dimensions.width > breakpointMobile) {
+      animateLine =
+        position === place ? { width: ["calc(15px)", "calc(117px)"] } : "";
+    } else {
+      animateLine =
+        position === place ? { width: ["calc(10%)", "calc(100%)"] } : "";
+    }
+
+    return (
+      <motion.div
+        type="button"
+        transition={transition}
+        animate={animateLine}
+        aria-label={`clickable info ${place}`}
+        styleName={getClassName(place)}
+        style={{ "--top": place }}
+        onClick={() => reset(place)}
+      />
+    );
+  }
+
+  function renderDesktop() {
+    return (
+      <>
+        <div styleName="copy-section">
+          <div styleName="lines">
+            {renderLine(transitionLine, 0)}
+            {renderLine(transitionLine, 1)}
+            {renderLine(transitionLine, 2)}
+          </div>
+
+          {textPlaceholder.map((text, index) => (
+            <div
+              key={`${text.message}_${text.title}`}
+              styleName="message-content"
+              style={{ display: display(index) }}
+            >
+              <motion.div
+                styleName="message"
+                transition={transitionMessage}
+                animate={animateMessage(index)}
+              >
+                <Typography color="oxford-blue">{text.message}</Typography>
+              </motion.div>
+
+              <div styleName="hide-message" />
+
+              <motion.div
+                styleName="message-title"
+                transition={transitionTitle}
+                initial={initialTitle}
+                animate={animateTitle(index)}
+              >
+                <Typography color="klein-blue" weight="bold" variant="h2">
+                  {text.title}
+                </Typography>
+              </motion.div>
+
+              <div styleName="hide-title" />
+            </div>
+          ))}
+
+          <div styleName="hide-title" />
+        </div>
+      </>
+    );
+  }
+
+  function renderMobile() {
+    return (
+      <>
+        <div styleName="copy-section">
+          <div styleName="lines">
+            {renderLine(transitionLineMobile, 0)}
+            {renderLine(transitionLineMobile, 1)}
+            {renderLine(transitionLineMobile, 2)}
+          </div>
+
+          <div styleName="slider">
+            {textPlaceholder.map((text, index) => (
+              <div
+                key={`${text.message}_${text.title}`}
+                styleName="message-content"
+                style={{ display: display(index) }}
+              >
+                <motion.div
+                  styleName="message"
+                  transition={{ ...transitionMessage, ease: "easeInOut" }}
+                  animate={animateMessage(index)}
+                >
+                  <Typography color="oxford-blue">{text.message}</Typography>
+                </motion.div>
+
+                <div styleName="hide-message" />
+
+                <motion.div
+                  styleName="message-title"
+                  transition={transitionTitleMobile}
+                  initial={initialTitle}
+                  animate={animateTitle(index)}
+                >
+                  <Typography color="klein-blue" weight="bold" variant="h2">
+                    {text.title}
+                  </Typography>
+                </motion.div>
+
+                <div styleName="hide-title" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </>
+    );
+  }
+
+  return dimensions.width > breakpointMobile ? renderDesktop() : renderMobile();
 }
 
 export default HeroCopyElement;
