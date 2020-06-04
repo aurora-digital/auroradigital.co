551f81492257afb8b8b946067314462e5983f4e6
Tânia Rocha <taniapassosrocha@gmail.com>
2020-06-04 18:18:14 +0100

    wip

--- src/sections/HomeServices/index.js
+++ src/sections/HomeServices/index.js
@@ -1,13 +1,10 @@
-import React, { useRef, useState, useLayoutEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import useScroll from "root/hooks/useScroll";
-import { pull, indexOf, isEmpty, cloneDeep } from "lodash-es";
-
 import Section from "root/components/Section";
 import Typography from "root/components/Typography";
 import ParallaxEffect from "root/components/ParallaxEffect";
-import { InView } from "react-intersection-observer";
-import classNames from "classnames";
+import { map } from "lodash-es";
+
 import withQuery from "./withQuery";
 
 import "./index.css";
@@ -22,129 +19,51 @@ const renderBlock = (title, subtitle, points) => {
         <Typography variant="h3" color="white">
           {subtitle}
         </Typography>
-        <Typography variant="small-body" color="white">
-          {points}
-        </Typography>
+        <div styleName="points">
+          {map(points, point => {
+            return (
+              <Typography variant="small-body" color="white">
+                {point}
+              </Typography>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
 };
 
 const HomeServices = ({ data }) => {
-  const [inView, setInView] = useState([]);
-  const [blockVisible, setBlockVisible] = useState(null);
-  const scrollPosition = useScroll();
-  const conceptRef = useRef();
-  const strategyRef = useRef();
-
-  const handleClickConcept = () => {
-    // setInView({ service: "concept" });
-  };
-
-  const handleChangeConcept = intersecting => {
-    const newInView = cloneDeep(inView);
-
-    if (intersecting) {
-      newInView.push("concept");
-    } else {
-      pull(newInView, "concept");
-    }
-
-    setInView(newInView);
-  };
-
-  const handleClickStrategy = () => {
-    // setInView("strategy");
-  };
-
-  const handleChangeStrategy = intersecting => {
-    const newInView = cloneDeep(inView);
-
-    if (intersecting) {
-      newInView.push("strategy");
-    } else {
-      pull(newInView, "strategy");
-    }
-
-    setInView(newInView);
-  };
-
-  const isBetweenPositions = (ref, space, percentage) => {
-    const initial = ref.current.node.offsetTop - space;
-    const end = initial + ref.current.node.offsetHeight * percentage;
-
-    if (scrollPosition >= initial && scrollPosition < end) {
-      return true;
-    }
-
-    return false;
-  };
-
-  useLayoutEffect(() => {
-    if (!isEmpty(inView)) {
-      if (
-        indexOf(inView, "strategy") !== -1 &&
-        isBetweenPositions(strategyRef, 400, 0.4)
-      ) {
-        setBlockVisible("strategy");
-      } else if (
-        indexOf(inView, "concept") !== -1 &&
-        isBetweenPositions(conceptRef, 50, 0.5)
-      ) {
-        setBlockVisible("concept");
-      } else {
-        setBlockVisible(null);
-      }
-    }
-  }, [scrollPosition, inView]);
-
   return (
     <Section>
       <div styleName="root">
         <div styleName="concept-stategy">
           <div styleName="concept">
-            <InView
-              as="div"
-              styleName={classNames("image", {
-                "concept-image": true,
-                selected: blockVisible === "concept",
-              })}
-              onClick={handleClickConcept}
-              onChange={handleChangeConcept}
-              ref={conceptRef}
-            >
+            <div styleName="image concept-image">
               <ParallaxEffect
                 image={data.concept.image.fluid}
-                alt="our team on a meeting"
+                alt="concept service"
               />
-              {renderBlock(
-                "We plan your",
-                "Concept",
-                "Business objectives\nUser expectations\nThe scope of the project",
-              )}
-            </InView>
+              {renderBlock("We plan your", "Concept", [
+                "Business objectives",
+                "User expectations",
+                "The scope of the project",
+              ])}
+            </div>
           </div>
           <div styleName="strategy">
-            <InView
-              as="div"
-              styleName={classNames("image", {
-                "stategy-image": true,
-                selected: blockVisible === "strategy",
-              })}
-              onClick={handleClickStrategy}
-              onChange={handleChangeStrategy}
-              ref={strategyRef}
-            >
+            <div styleName="image stategy-image">
               <ParallaxEffect
                 image={data.stategy.image.fluid}
-                alt="developers working on a whiteboard"
+                alt="stategy service"
               />
-              {renderBlock(
-                "We define your",
-                "Strategy",
-                "Product vision\nDesign Sprint\nMarket research\nRequirements definition",
-              )}
-            </InView>
+              {renderBlock("We define your", "Strategy", [
+                "Product vision",
+                "Design Sprint",
+                "Market research",
+                "Requirements definition",
+              ])}
+            </div>
           </div>
         </div>
         <div styleName="code-design-growth">
@@ -152,23 +71,54 @@ const HomeServices = ({ data }) => {
             <div styleName="image code-image">
               <ParallaxEffect
                 image={data.code.image.fluid}
-                alt="our team on a meeting"
+                alt="code service"
               />
+              {renderBlock("We build your", "Code", [
+                "Web & Mobile",
+                "Integrations",
+                "Hosting",
+                "Deploy",
+              ])}
             </div>
           </div>
           <div styleName="design-growth">
             <div styleName="image design-image">
               <ParallaxEffect
                 image={data.design.image.fluid}
-                alt="our team on a meeting"
+                alt="design service"
               />
+              {renderBlock("We create your", "Design", [
+                "Content strategy",
+                "Prototyping",
+                "UX/UI",
+                "User testing",
+              ])}
             </div>
             <div styleName="image growth-image">
               <ParallaxEffect
                 image={data.growth.image.fluid}
-                alt="our team on a meeting"
+                alt="growth service"
               />
+              {renderBlock("We raise your", "Growth", [
+                "Product scaling",
+                "Conversion improvment",
+                "Performance",
+              ])}
             </div>
+          </div>
+        </div>
+        <div styleName="copy">
+          <div styleName="above-text">
+            {
+              "Healthcare solutions are quite sensitive and need to be built with 100% focus on the user’s needs, safety and privacy.\nWe work alongside our customers, from research to&nbsp;user-testing, to build excellent software."
+            }
+          </div>
+          <div styleName="under-text">
+            <Typography color="oxford-blue">
+              {
+                "Healthcare solutions are quite sensitive and need to be built with 100% focus on the user’s needs, safety and privacy.\nWe work alongside our customers, from research to\nuser-testing, to build excellent software."
+              }
+            </Typography>
           </div>
         </div>
       </div>
