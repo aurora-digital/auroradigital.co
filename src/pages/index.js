81ef06c729cf7b4e8ca61a665c89e694d0a8763c
Tânia Rocha <taniapassosrocha@gmail.com>
2020-06-17 12:50:27 +0100

    Add working together section

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Layout from "root/components/Layout";
 import Footer from "root/components/Footer";
 import HomeHero from "root/sections/HomeHero";
 import HomePortfolio from "root/sections/HomePortfolio";
-import HomeMission from "root/sections/HomeMission";
+import HomeWorkingTogether from "root/sections/HomeWorkingTogether";
 import HomeServices from "root/sections/HomeServices";
 import LetsTalk from "root/sections/LetsTalk";
 import HomeOurProcess from "root/sections/HomeOurProcess";
@@ -20,7 +20,7 @@ export default class IndexPage extends Component {
         <HomeServices />
         <HomeOurProcess />
         <HomePortfolio />
-        <HomeMission />
+        <HomeWorkingTogether />
         <LetsTalk />
         <Footer />
       </Layout>
