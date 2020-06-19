f9d65ab136bb30d36a21ae027ebb546999132688
Tânia Rocha <taniapassosrocha@gmail.com>
2020-06-19 11:08:49 +0100

    Update services section

--- src/sections/HomeServices/index.js
+++ src/sections/HomeServices/index.js
@@ -3,15 +3,21 @@ import PropTypes from "prop-types";
 import Section from "root/components/Section";
 import Typography from "root/components/Typography";
 import ParallaxEffect from "root/components/ParallaxEffect";
+import updateDimensions from "root/hooks/useWindowDimensions";
 import { map } from "lodash-es";
 import classNames from "classnames";
+import { Parallax } from "react-scroll-parallax";
 
 import withQuery from "./withQuery";
 
 import "./index.css";
 
+const breakpointMobile = 800;
+
 const HomeServices = ({ data }) => {
-  const renderBlock = (title, subtitle, points) => {
+  const dimensions = updateDimensions();
+
+  const renderBlockContent = (name, title, subtitle, serviceTopics) => {
     return (
       <div styleName="block">
         <div styleName="block-content">
@@ -22,10 +28,14 @@ const HomeServices = ({ data }) => {
             {subtitle}
           </Typography>
           <div styleName="points">
-            {map(points, point => {
+            {map(serviceTopics, (topic, index) => {
               return (
-                <Typography variant="small-body" color="white">
-                  {point}
+                <Typography
+                  key={`${name}-${index}`}
+                  variant="small-body"
+                  color="white"
+                >
+                  {topic}
                 </Typography>
               );
             })}
@@ -35,43 +45,63 @@ const HomeServices = ({ data }) => {
     );
   };
 
+  const renderBlock = (
+    name,
+    title,
+    subtitle,
+    serviceTopics,
+    parallaxY,
+    parallaxX,
+  ) => {
+    if (parallaxX) {
+      return (
+        <div styleName="parallax-container">
+          <Parallax key={`${name}-block`} x={parallaxX}>
+            {renderBlockContent(name, title, subtitle, serviceTopics)}
+          </Parallax>
+        </div>
+      );
+    }
+
+    return (
+      <Parallax key={`${name}-block`} y={parallaxY}>
+        {renderBlockContent(name, title, subtitle, serviceTopics)}
+      </Parallax>
+    );
+  };
+
   const renderService = (
     name,
     image,
     blockTitle,
     blockSubtitle,
     blockPoints,
+    parallaxBlockY,
+    parallaxBlockX,
   ) => {
     return (
-      <button
-        key={name}
-        type="button"
+      <div
+        key={`${name}-image`}
         styleName={classNames("image", {
           [`${name}-image`]: true,
         })}
       >
         <ParallaxEffect image={image} alt={`${name} service`} />
-        {renderBlock(blockTitle, blockSubtitle, blockPoints)}
-      </button>
+        {renderBlock(
+          name,
+          blockTitle,
+          blockSubtitle,
+          blockPoints,
+          parallaxBlockY,
+          parallaxBlockX,
+        )}
+      </div>
     );
   };
 
   return (
-    <Section>
-      <div styleName="concept-strategy">
-        <div styleName="concept">
-          {renderService(
-            "concept",
-            data.concept.image.fluid,
-            "We plan your",
-            "Concept",
-            [
-              "Business objectives",
-              "User expectations",
-              "The scope of the project",
-            ],
-          )}
-        </div>
+    <Section verticalSpacing={false}>
+      <div styleName="services">
         <div styleName="strategy">
           {renderService(
             "strategy",
@@ -84,10 +114,37 @@ const HomeServices = ({ data }) => {
               "Market research",
               "Requirements definition",
             ],
+            null,
+            dimensions.width > breakpointMobile ? [20, -20] : [10, -10],
           )}
         </div>
-      </div>
-      <div styleName="code-design-growth">
+        <div styleName="concept">
+          {renderService(
+            "concept",
+            data.concept.image.fluid,
+            "We plan your",
+            "Concept",
+            [
+              "Business objectives",
+              "User expectations",
+              "The scope of the project",
+            ],
+            null,
+            [-16, 16],
+          )}
+        </div>
+
+        <div styleName="design">
+          {renderService(
+            "design",
+            data.design.image.fluid,
+            "We create your",
+            "Design",
+            ["Content strategy", "Prototyping", "UX/UI", "User testing"],
+            [17, -17],
+            null,
+          )}
+        </div>
         <div styleName="code">
           {renderService(
             "code",
@@ -95,46 +152,34 @@ const HomeServices = ({ data }) => {
             "We build your",
             "Code",
             ["Web & Mobile", "Integrations", "Hosting", "Deploy"],
+            [-23, 23],
+            null,
           )}
         </div>
-
-        <div styleName="design-growth">
-          {renderService(
-            "design",
-            data.design.image.fluid,
-            "We create your",
-            "Design",
-            ["Content strategy", "Prototyping", "UX/UI", "User testing"],
-          )}
-          {renderService(
-            "code",
-            data.code.image.fluid,
-            "We build your",
-            "Code",
-            ["Web & Mobile", "Integrations", "Hosting", "Deploy"],
-          )}
-
+        <div styleName="growth">
           {renderService(
             "growth",
             data.growth.image.fluid,
             "We raise your",
             "Growth",
             ["Product scaling", "Conversion improvment", "Performance"],
+            null,
+            dimensions.width > breakpointMobile ? [20, -20] : [10, -10],
           )}
         </div>
-      </div>
-      <div styleName="copy">
-        <div styleName="above-text">
-          {
-            "Healthcare solutions are quite sensitive and need to be built with 100% focus on the user’s needs, safety and privacy.\nWe work alongside our customers, from research to&nbsp;user-testing, to build excellent software."
-          }
-        </div>
-        <div styleName="under-text">
-          <Typography color="oxford-blue">
+        <div styleName="copy">
+          <div styleName="above-text">
             {
-              "Healthcare solutions are quite sensitive and need to be built with 100% focus on the user’s needs, safety and privacy.\nWe work alongside our customers, from research to\nuser-testing, to build excellent software."
+              "Healthcare solutions are quite sensitive and need to be built with 100% focus on the user’s needs, safety and privacy.\nWe work alongside our customers, from research to&nbsp;user-testing, to build excellent software."
             }
-          </Typography>
+          </div>
+          <div styleName="under-text">
+            <Typography color="oxford-blue">
+              {
+                "Healthcare solutions are quite sensitive and need to be built with 100% focus on the user’s needs, safety and privacy.\nWe work alongside our customers, from research to\nuser-testing, to build excellent software."
+              }
+            </Typography>
+          </div>
         </div>
       </div>
     </Section>
